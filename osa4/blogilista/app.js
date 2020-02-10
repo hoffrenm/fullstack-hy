cd7d912393553e26e92ee06f178eba161d2fccf1
Mika Hoffren <mika.hoffren@helsinki.fi>
2020-02-10 17:41:48 +0200

    finish exercises 4.1-4.2

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,21 @@
+const config = require('./utils/config')
+const mongoose = require('mongoose')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(error => console.log(`Error connecting to MongoDB: ${error.message}`))
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
