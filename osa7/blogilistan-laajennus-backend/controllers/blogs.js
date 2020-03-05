6b3ea712e75a7f94b6e707fd1b03f92c20a92033
Mika Hoffren <mika.hoffren@helsinki.fi>
2020-03-05 14:34:43 +0200

    finish backend comment functionality for exercises 7.17-7.18

--- osa7/blogilistan-laajennus-backend/controllers/blogs.js
+++ osa7/blogilistan-laajennus-backend/controllers/blogs.js
@@ -2,13 +2,16 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const jwt = require('jsonwebtoken')
 const User = require('../models/user')
+const Comment = require('../models/comment')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({}).populate('user', {
-    username: 1,
-    name: 1,
-    id: 1
-  })
+  const blogs = await Blog.find({})
+    .populate('user', {
+      username: 1,
+      name: 1,
+      id: 1
+    })
+    .populate('comments', { content: 1, id: 1 })
 
   response.json(blogs.map(blog => blog.toJSON()))
 })
@@ -43,6 +46,36 @@ blogsRouter.post('/', async (request, response) => {
   response.status(201).json(savedBlog.toJSON())
 })
 
+blogsRouter.post('/:id/comments', async (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).end()
+  }
+
+  const blog = await Blog.findById(request.params.id)
+
+  if (!blog) {
+    return response.status(400).end()
+  }
+
+  const comment = new Comment({
+    content: body.content
+  })
+
+  await comment.save()
+
+  blog.comments = blog.comments.concat(comment._id)
+
+  const savedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true
+  })
+    .populate('user', { id: 1, name: 1, username: 1 })
+    .populate('comments', { id: 1, content: 1 })
+
+  response.status(201).json(savedBlog.toJSON())
+})
+
 blogsRouter.delete('/:id', async (request, response) => {
   const token = request.token
   const decodedToken = jwt.verify(token, process.env.SECRET)
@@ -64,9 +97,14 @@ blogsRouter.delete('/:id', async (request, response) => {
 
 blogsRouter.put('/:id', async (request, response) => {
   const blog = { ...request.body }
+
   const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
     new: true
   })
+    .populate('user', { id: 1, username: 1, name: 1 })
+    .populate('comments', { id: 1, content: 1 })
+
+  console.log('updated: ', updatedBlog)
 
   response.json(updatedBlog.toJSON())
 })
